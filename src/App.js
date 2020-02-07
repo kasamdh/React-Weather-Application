8ad6a00e4d06f28f24f31a455316451e7f6f9eea
Kasam <kasamdh@hotmail.com>
2020-02-06 20:29:23 -0500

    Form, Titles and Weather component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ // importing from package.json 
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "1b41d6f15e637580fd1e98a9eb3dc363";
+
+
+//state is an object lives within the component and responsible for changing data within the component
+// state carry Key Value Pairs
+ class App extends React.Component {
+   
+   state = {
+     temperature:undefined,
+     city:undefined,
+     country:undefined,
+     humidity:undefined,
+     description:undefined,
+     error:undefined
+     
+   }
+   getWeather = async(e)=>{
+    e.preventDefault();
+    //target.elements  for city and its value whatever we type into the input
+     const city = e.target.elements.city.value;
+     //target.elements  for country and its value whatever we type into the input
+     const country = e.target.elements.country.value;
+     // http calls web 
+    //  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+      // const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=${API_KEY}`);
+   const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+   
+     // convert API to JSON format
+     const data = await api_call.json();
+     console.log(data)
+     
+     // setState is a builtin method
+     this.setState({
+       temperature:data.main.temp,
+     });
+
+
+
+   }
+  render() {
+    return (
+      <div>
+        <Titles/>
+        <Form getWeather = {this.getWeather}/>
+        <Weather/>
+       
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/components/Weather.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Weather extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Component</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default Weather
