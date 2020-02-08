b129401397932b4052b0069778702d30e0f6bab3
Kasam <kasamdh@hotmail.com>
2020-02-07 20:45:29 -0500

    Css Added and Image upload

--- src/App.js
+++ src/App.js
@@ -72,22 +72,39 @@ const API_KEY = "1b41d6f15e637580fd1e98a9eb3dc363";
   render() {
     return (
       <div>
-        <Titles/>
-        <Form getWeather = {this.getWeather}/>
-        <Weather
-        // this are the properties aka Props
-            temperature= {this.state.temperature}
-            city = {this.state.city}
-            country = {this.state.country}
-            humidity = {this.state.humidity}
-            description = {this.state.description}
-            error = {this.state.error}
+        <div className= "wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                <Titles/>
 
+                </div>
+                <div className="col-xs-7 form-container">
+                <Form getWeather = {this.getWeather}/>
+                    <Weather
+                        // this are the properties aka Props
+                      temperature= {this.state.temperature}
+                      city = {this.state.city}
+                      country = {this.state.country}
+                      humidity = {this.state.humidity}
+                      description = {this.state.description}
+                      error = {this.state.error}
+                      />
+                
 
-            
-            
-            />
-       
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+        
+        
+        
         
       </div>
     )
