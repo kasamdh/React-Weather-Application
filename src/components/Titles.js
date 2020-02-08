7c3b834f9a5cc9df910902e9d0df7e90e3ef852b
Kasam <kasamdh@hotmail.com>
2020-02-07 21:01:19 -0500

    CSS and Image

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,9 @@ export class Titles extends Component {
     render() {
         return (
             <div>
-                <h1>Weather Finder</h1>
+                <h1>World Weather Finder</h1>
                 <p>Find out temperature, conditions and more</p>
+                <p>~Kasam Dhakal</p>
                 
             </div>
         )
