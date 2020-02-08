6c45df94057b4a07f8b321e2d8b956780b35b1e9
Kasam <kasamdh@hotmail.com>
2020-02-07 20:09:30 -0500

    weather api complete

--- src/App.js
+++ src/App.js
@@ -22,23 +22,49 @@ const API_KEY = "1b41d6f15e637580fd1e98a9eb3dc363";
    }
    getWeather = async(e)=>{
     e.preventDefault();
-    //target.elements  for city and its value whatever we type into the input
+        //target.elements  for city and its value whatever we type into the input
      const city = e.target.elements.city.value;
-     //target.elements  for country and its value whatever we type into the input
+        //target.elements  for country and its value whatever we type into the input
      const country = e.target.elements.country.value;
-     // http calls web 
-    //  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-      // const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=${API_KEY}`);
-   const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+        // http calls web 
+        //  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        // const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
    
      // convert API to JSON format
      const data = await api_call.json();
-     console.log(data)
+    //  console.log(data)
      
      // setState is a builtin method
-     this.setState({
-       temperature:data.main.temp,
-     });
+     // to access the data we need data and anything we want to access
+ 
+    //If both cities and country is true show the value
+     if (city && country){
+      this.setState({
+        //data.main.temp is coming from API console
+         temperature:data.main.temp,
+         city:data.name,
+         country:data.sys.country,
+         humidity:data.main.humidity,
+         description:data.weather[0].description,
+         error:""
+ 
+      });
+
+
+     }else{
+      this.setState({
+        //data.main.temp is coming from API console
+         temperature:undefined,
+         city:undefined,
+         country:undefined,
+         humidity:undefined,
+         description:undefined,
+         error:'Please Enter the Values'
+ 
+      });
+
+     }
 
 
 
@@ -48,7 +74,19 @@ const API_KEY = "1b41d6f15e637580fd1e98a9eb3dc363";
       <div>
         <Titles/>
         <Form getWeather = {this.getWeather}/>
-        <Weather/>
+        <Weather
+        // this are the properties aka Props
+            temperature= {this.state.temperature}
+            city = {this.state.city}
+            country = {this.state.country}
+            humidity = {this.state.humidity}
+            description = {this.state.description}
+            error = {this.state.error}
+
+
+            
+            
+            />
        
         
       </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,17 @@ export class Weather extends Component {
     render() {
         return (
             <div>
-                <h1>Weather Component</h1>
+                {/* now we use condition */}
+                {/* <p>Location: {this.props.city}, {this.props.country}</p> */}
+                {/* <p>Temperature: {this.props.temperature}</p>
+                <p>Humidity: {this.props.humidity}</p>
+                <p>Condition: {this.props.description}</p> */}
+                {this.props.city && this.props.country &&<p>Location: {this.props.city}, {this.props.country}</p> }
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
+                {this.props.description &&   <p>Condition: {this.props.description}</p>}
+                {this.props.error && <p>{this.props.error}</p>}
+
                 
             </div>
         )
