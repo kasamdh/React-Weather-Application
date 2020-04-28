d26bf0717f5ac81a7df55d1c15ecffc08ed50d8e
Kasam <kasamdh@hotmail.com>
2020-04-28 18:02:55 -0500

    component change

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ const API_KEY = "1b41d6f15e637580fd1e98a9eb3dc363";
 // state carry Key Value Pairs
  class App extends React.Component {
    
-  //  state = {
-  //    temperature:undefined,
-  //    city:undefined,
-  //    country:undefined,
-  //    humidity:undefined,
-  //    description:undefined,
-  //    error:undefined
+   state = {
+     temperature:undefined,
+     city:undefined,
+     country:undefined,
+     humidity:undefined,
+     description:undefined,
+     error:undefined
      
-  //  }
+   }
    getWeather = async(e)=>{
     e.preventDefault();
         //target.elements  for city and its value whatever we type into the input
